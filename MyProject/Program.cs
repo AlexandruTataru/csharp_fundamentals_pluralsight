20330aca24bc462054ac7376fee505cb15397a34
TataruAlexandru <alexandruflavian@gmail.com>
2025-11-12 20:12:01 +0200

    Added print of .NET version

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine($".NET version: {Environment.Version}");
         }
     }
 }
